f5b86f7c812e886cb9a02bfadf015a25767dc5d6
leon <leon@example.com>
2018-06-22 05:26:09 +0000

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Leon  Test ' });
+  res.render('index', { title: 'Leon  Test 111' });
 });
 
 module.exports = router;
